319dae6778a0f49697b7057632dcf4b5ce54f0aa
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-05-25 21:21:18 +0200

    \update\

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,13 +10,63 @@
 // }
 
 
-function cardFlip() {
+function cardFlip1() {
     document.getElementById("cardRevealFace").classList.add("reveal-face");
     console.log("face");
-}
-
-function cardFlip2() {
     document.getElementById("cardRevealBack").classList.add("reveal-back");
     console.log("back");
-
+}
+function cardFlip2() {
+    document.getElementById("cardRevealFace2").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack2").classList.add("reveal-back");
+    console.log("back");
+}
+function cardFlip3() {
+    document.getElementById("cardRevealFace3").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack3").classList.add("reveal-back");
+    console.log("back");
+}
+function cardFlip4() {
+    document.getElementById("cardRevealFace4").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack4").classList.add("reveal-back");
+    console.log("back");
+}
+function cardFlip5() {
+    document.getElementById("cardRevealFace5").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack5").classList.add("reveal-back");
+    console.log("back");
+}
+function cardFlip6() {
+    document.getElementById("cardRevealFace6").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack6").classList.add("reveal-back");
+    console.log("back");
+}
+function cardFlip7() {
+    document.getElementById("cardRevealFace7").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack7").classList.add("reveal-back");
+    console.log("back");
+}
+function cardFlip8() {
+    document.getElementById("cardRevealFace8").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack8").classList.add("reveal-back");
+    console.log("back");
+}
+function cardFlip9() {
+    document.getElementById("cardRevealFace9").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack9").classList.add("reveal-back");
+    console.log("back");
+}
+function cardFlip10() {
+    document.getElementById("cardRevealFace10").classList.add("reveal-face");
+    console.log("face");
+    document.getElementById("cardRevealBack10").classList.add("reveal-back");
+    console.log("back");
 }
